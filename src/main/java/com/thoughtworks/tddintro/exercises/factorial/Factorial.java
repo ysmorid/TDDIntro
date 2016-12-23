323bd0f0fa02b60e615d0825b15720e13a134f24
Ylia Morid <Ylia Morid>
2016-12-22 20:24:07 -0800

    added a throw new IllegalArgumentException() for negative numbers

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,7 +2,8 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        if(i < 1){
+        if(i < 0) throw new IllegalArgumentException ("I'm sorry, that is an invalid number. Only positives integers allowed.");
+        else if(i == 0){
             return 1;
         }
         else{

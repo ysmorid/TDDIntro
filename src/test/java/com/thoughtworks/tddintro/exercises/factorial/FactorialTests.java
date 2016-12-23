17319c7e5c66a3a620cbf00a3a9461d311387990
Ylia Morid <Ylia Morid>
2016-12-22 20:22:30 -0800

    removed @ignore and comments from shouldThrowIllegalArgumentExceptionWhenNumberIsNegative()

--- src/test/java/com/thoughtworks/tddintro/exercises/factorial/FactorialTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/factorial/FactorialTests.java
@@ -1,5 +1,4 @@
 package com.thoughtworks.tddintro.exercises.factorial;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
@@ -43,7 +42,6 @@ public class FactorialTests {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    @Ignore // Remove each @Ignore to allow the test to run
     public void shouldThrowIllegalArgumentExceptionWhenNumberIsNegative(){
 
         factorial.compute(-1);

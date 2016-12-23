71d49213b2a3b86bf27afdbbc79ee7c03e06da68
Ylia Morid <Ylia Morid>
2016-12-22 17:27:22 -0800

    created withdrawFunds() to withdraw passed int from funds and return new value

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,10 +1,17 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
-    public int addFunds(int money) {
+    public int addFunds(int deposit) {
         int funds = 0;
 
-        funds += money;
+        funds += deposit;
+        return funds;
+    }
+
+    public int withdrawFunds(int withdraw) {
+        int funds = 10;
+
+        funds-= withdraw;
         return funds;
     }
 }

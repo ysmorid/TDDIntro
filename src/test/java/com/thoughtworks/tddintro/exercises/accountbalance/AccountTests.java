c52e0d87997995320c3b693496e47c7ec70b954b
Ylia Morid <Ylia Morid>
2016-12-22 19:39:53 -0800

    removed comments

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -7,21 +7,18 @@ import static org.hamcrest.core.Is.is;
 
 public class AccountTests {
     @Test
-    // Remove each @Ignore and implement test
     public void shouldIncreaseMyBalanceWhenIDepositMoney() {
         Account account = new Account();
         assertThat(account.addFunds(10), is(10));
     }
 
     @Test
-    // Remove each @Ignore and implement test
     public void shouldDecreaseMyBalanceWhenIWithdrawMoney() {
         Account account = new Account();
         assertThat(account.withdrawFunds(10), is(0));
     }
 
     @Test
-    // Remove each @Ignore and implement test
     public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal() {
         Account account = new Account();
         assertThat(account.withdrawFunds(20), is(10));

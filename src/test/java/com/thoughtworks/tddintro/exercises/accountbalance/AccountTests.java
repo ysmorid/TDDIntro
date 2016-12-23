47983523dc44f1d7186e5ac6e8598a73c1a87892
Ylia Morid <Ylia Morid>
2016-12-22 17:21:06 -0800

    shouldIncreaseMyBalanceWhenIDepositMoney() added

--- src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
+++ src/test/java/com/thoughtworks/tddintro/exercises/accountbalance/AccountTests.java
@@ -3,22 +3,27 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class AccountTests {
     @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldIncreaseMyBalanceWhenIDepositMoney(){
+    // Remove each @Ignore and implement test
+    public void shouldIncreaseMyBalanceWhenIDepositMoney() {
         Account account = new Account();
-    }
-
-    @Test
-    @Ignore  // Remove each @Ignore and implement test
-    public void shouldDecreaseMyBalanceWhenIWithdrawMoney(){
+        assertThat(account.addFunds(10), is(10));
 
     }
 
     @Test
     @Ignore  // Remove each @Ignore and implement test
-    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal(){
+    public void shouldDecreaseMyBalanceWhenIWithdrawMoney() {
+
+    }
+
+    @Test
+    @Ignore  // Remove each @Ignore and implement test
+    public void shouldNotDecreaseMyBalanceWhenIWithdrawMoneyAndDoNotHaveEnoughToCoverTheWithdrawal() {
 
     }
 }

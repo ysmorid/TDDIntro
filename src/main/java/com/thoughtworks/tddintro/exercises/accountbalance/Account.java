3db5252bc75a724d72e602f02ce6349f90bb2698
Ylia Morid <Ylia Morid>
2017-01-05 19:56:38 -0800

    added displayFunds() method to return int funds. Converted withdrawFunds() and addFunds() to void methods.

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -3,18 +3,20 @@ package com.thoughtworks.tddintro.exercises.accountbalance;
 public class Account {
     int funds = 0;
 
-    public int addFunds(int deposit) {
+    public void addFunds(int deposit) {
         funds += deposit;
-        return funds;
     }
 
-    public int withdrawFunds(int withdraw) {
+    public void withdrawFunds(int withdraw) {
         if(withdraw > funds){
             System.out.println("I'm sorry, you do not have enough to withdraw. Please select " + "$" + funds + " or less.");
         }
         else{
             funds -= withdraw;
         }
+    }
+
+    public int displayFunds(){
         return funds;
     }
 }

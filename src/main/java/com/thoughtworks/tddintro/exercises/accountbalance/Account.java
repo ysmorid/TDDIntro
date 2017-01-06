881e0f98009f3e97cb96c619192293fb6db957ec
Ylia Morid <Ylia Morid>
2017-01-05 19:43:34 -0800

    initialized int funds in Account class

--- src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/accountbalance/Account.java
@@ -1,20 +1,19 @@
 package com.thoughtworks.tddintro.exercises.accountbalance;
 
 public class Account {
-    public int addFunds(int deposit) {
-        int funds = 0;
+    int funds = 0;
 
+    public int addFunds(int deposit) {
         funds += deposit;
         return funds;
     }
 
     public int withdrawFunds(int withdraw) {
-        int funds = 10;
         if(withdraw > funds){
             System.out.println("I'm sorry, you do not have enough to withdraw. Please select " + "$" + funds + " or less.");
         }
         else{
-            funds-= withdraw;
+            funds -= withdraw;
         }
         return funds;
     }
